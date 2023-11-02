40fe6be2ccc0d2707dc9097d317590ccbf65d516
Lucas Acosta <lucas.heck@live.com>
2023-11-01 22:58:32 -0300

    single file completed project

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,83 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { def } from '@vue/shared';
+import { reactive } from 'vue';
+
+  const estado = reactive({
+    x: 0,
+    y: 0,
+    operacao: 'add',
+    resultado: '',
+  })
+
+  const calc = () => 
+  {
+    if(typeof estado.x === 'number' && typeof estado.y === 'number' )
+    {
+      switch(estado.operacao){
+        case 'add':
+          estado.resultado = estado.x + estado.y;
+          break;
+        case 'subtract':
+          estado.resultado = estado.x - estado.y;
+          break;
+        case 'multiply':
+          estado.resultado = estado.x * estado.y;
+          break;
+        case 'divide':
+          if(estado.y !== 0){
+            estado.resultado = estado.x / estado.y;
+          }else {
+            estado.resultado = ''
+          }
+          break;
+        default:
+          console.log("algo deu errado");
+          estado.resultado = '';
+          break;
+      }
+    }
+    else
+    {
+      estado.resultado = '';
+    }
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container">
+    <header class="p-3 mb-2 mt-4 bg-primary rounded-3 text-center">
+      <h1>Calculadora em Vue.js</h1>
+      <p>
+        Faça seu cálculo:
+      </p>
+    </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <form>
+      <div class="row">
+        <div class="col-md-5">
+          <input v-model="estado.x" @input="calc" type="number" placeholder="Número 1" class="form-control">
+        </div>
+        <div class="col-md-2">
+          <select v-model="estado.operacao" @change="calc" class="form-control">
+            <option value="add">+</option>
+            <option value="subtract">-</option>
+            <option value="multiply">x</option>
+            <option value="divide">/</option>
+          </select>
+        </div>
+        <div class="col-md-5">
+          <input v-model="estado.y" @input="calc" type="number" placeholder="Número 2" class="form-control">
+        </div>
+      </div>
+      <div class="row p-5">
+        <div class="col-md-12 text-center">
+          <h3>Resultado:</h3>
+          <h3 class="resultado">{{ estado.resultado }}</h3>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
